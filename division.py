9acf2e1ee841c67971efaa8706e61509641f935e
Daria-Iulia Berce <daria.berce1@gmail.com>
2023-12-21 20:31:57 +0000

    Create division.py

---
+++ division.py
@@ -0,0 +1,17 @@
+t=int(input())
+for i in range(t):
+    rating=int(input())
+    
+    if rating<=1399:
+        print("Division 4")
+        
+    elif 1400<=rating<=1599:
+        print("Division 3")
+        
+    elif 1600<=rating<=1899:
+        print("Division 2")
+        
+    elif 1900<=rating:
+        print("Division 1")
+        
+        
